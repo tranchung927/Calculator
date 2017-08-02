9a85a9d0716d66e7a16b7c63ac156dde0aa0f470
TranChung <tranchung927@gmail.com>
2017-08-02 08:46:05 +0700

    init

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Admin on 8/2/17.
+//  Copyright © 2017 ChungSama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+    
+    }
+}
+
